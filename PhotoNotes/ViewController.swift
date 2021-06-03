99fde6dd63ff46f12c3328e61219be0ef09098e7
Azat <kaumov.a.r@gmail.com>
2021-06-03 21:18:21 +0300

    Initial commit

---
+++ PhotoNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoNotes
+//
+//  Created by Azat Kaiumov on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
